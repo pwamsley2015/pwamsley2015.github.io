520f498d200b9f76e1ab29842a1996f999eeefc5
Patrick Wamsley <pwamsley2015@francisparker.org>
2020-06-22 15:05:09 -0700

    Making progress

--- src/index.js
+++ src/index.js
@@ -1,18 +1,116 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from 'react-bootstrap/Button'
-import './index.css';
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
 import RoundedImage from "react-rounded-image";
 import SidebarImage from './images/logo192.png';
+import TempImage from './images/temp.png';
 import {Container, Row, Col} from 'react-bootstrap';
+import Sidebar from "react-sidebar";
 
-const INTRO = "Welcome!"
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function arrFormat(a) {
+  let result = "" + a[0];
+  for (let i = 1; i < a.length; i++) {
+    result += ", " + a[i];
+  }
+  return result;
+}
+
+const ABOUT = {
+  INTRO: "Welcome! I'm a Software Engineer, primaryly interested in backend development, but I'm never afraid to learn new technologies. I love coding, math, and heavy weight.",
+  HEADLINE: "Software Engineer",
+  EMAIL: "pwamsley2019@gmail.com",
+  GITHUB: "pwamsley2015.github.com",
+  INSTA: "https://www.instagram.com/patrick_wamsley/",
+};
+
+const PROJECTS = [
+  { name: "Friday",
+    code: "https://github.com/pwamsley2015/public_friday",
+    about: "My personal assistant chatbot which automates tasks, logs data, manages my schedule, and keeps me hacking. Dynamic IO over the web and local intranet.",
+    roles: ["Everything"], 
+    built_with: ["Twilio Messaging API", "Java", "Python", "Spark", "JavaFX"]
+  },
+  { name: "Congressional Redistricting",
+    code: "https://github.com/pwamsley2015/congressional_redistricting",
+    about: "Web application which generates congressional redistricting lines, built to maximize the number of majority-miniority distrcts.",
+    roles: ["Primary Backend Developer", "Designed and implemented AlogrithmStep framework to modularize Alogrithm", "Implemented Graph Partioning Algorithm to generate majority-miniority distrcts"],
+    built_with: ["React", "TypeScript", "Java", "Hibernate"]
+  },
+  {
+    name: "MedCluster",
+    code: "https://github.com/pwamsley2015/medcluster",
+    about: "Machine Learning and Data Visualization tool used to optimize EMT base station locations.",
+    roles: ["Primary Backend Developer", "Implemented k-means clustering Algorithm"],
+    built_with: ["Python", "Google Maps API", "JavaScript"]
+  },
+  {
+    name: "No Limit Hold'Em Equity Calculator",
+    code: "https://github.com/pwamsley2015/NLHE_lab",
+    about: "Calculates Range vs Range Equity in Poker.",
+    roles: ["Everything"],
+    built_with: ["Java", "JUnit", "Monti-Carlo Estimation"]
+  }, 
+  {
+    name: "FRC 2485 Robotics",
+    code: "https://github.com/team2485/frc-2016",
+    about: "Autonomous and Teleoperated Robot Control for FRC Recycle Rush",
+    roles: ["Lead Developer"],
+    built_with: ["Java", "WPI FRC Framework"]
+  }
+];
+
+class ProjectDisplay extends React.Component {
+  //props = project
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const project = this.props.project;
+    const l = "{";
+    const r = "}";
+    let rolesFormat = arrFormat(project.roles);
+    let builtWithFormat = arrFormat(project.built_with);
+
+    return (
+      <Card style={{ width: '18rem'}}>
+        <Card.Img src={TempImage} alt="Card image" />
+        <Card.ImgOverlay>
+          <Card.Body>
+            <Card.Title>{project.name}</Card.Title>
+            <Card.Text>{project.about}</Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Text>Roles: {rolesFormat}</Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <Card.Text>Built with: {builtWithFormat}</Card.Text>
+          </Card.Body>
+          <Card.Body>
+            <a href={project.code}>{l}code{r}</a>
+          </Card.Body>
+        </Card.ImgOverlay>
+      </Card>
+    );
+  }
+}
 
 class ProjectPage extends React.Component {
   render() {
+
+    const items = [];
+    for (var i = 0; i < PROJECTS.length; i++) {
+      items.push(<ProjectDisplay key={i} project={PROJECTS[i]}/>);
+    }
+
     return ( 
-      <div className="projects">
-      Projects
+      <div>
+        {items}
       </div>
     );
   }
@@ -33,18 +131,11 @@ class AboutSection extends React.Component {
   }
 }
 
-class SideBar extends React.Component {
+class SideBarContent extends React.Component {
   render() {
-    const sidebarStyle = {
-      color: "black",
-      backgroundColor: "white", 
-      padding: "10px",
-      fontFamily: "Arial",
-      // width: 200,
-      // height: 600,
-    }
+
     return (
-      <div className="sidebar" style={sidebarStyle}>
+      <div className="sidebar">
         <Container>
           <Col>
             <Row>
@@ -57,13 +148,9 @@ class SideBar extends React.Component {
             </Row>
            
             <Row>
-              <h3>Software Engineer</h3>
+              <h3>{ABOUT.HEADLINE}</h3>
             </Row>
-
-              <Row>B.S. Computer Science</Row>
-          
-            <Row>{INTRO}</Row>
-
+            <Row>{ABOUT.INTRO}</Row>
             <div> <b>Links</b> <br/> </div>
           </Col>
         </Container>
@@ -78,7 +165,7 @@ class Banner extends React.Component {
       <Container>
         <Col>
           <Row>
-            <h1> Patrick Wamsley </h1>
+            <h1>Patrick Wamsley</h1>
           </Row>
           <Row> 
             <Button variant="outline-primary">See Projects</Button>{' '} 
@@ -93,7 +180,11 @@ class HomeScreen extends React.Component {
   render() {
     return <div className="home_screen">
       <Banner /> 
-      <SideBar />
+        <Sidebar
+        sidebar={<SideBarContent/>}
+        docked={false}
+        styles={{ sidebar: { background: "white" } }}
+      />
       <AboutSection />
       <ProjectPage />
     </div>
