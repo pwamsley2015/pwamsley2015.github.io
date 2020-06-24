aa01df14e99c1909fc342c38d55b309c3714d40b
Patrick Wamsley <pwamsley2015@francisparker.org>
2020-06-23 21:50:39 -0700

    progress

--- src/index.js
+++ src/index.js
@@ -3,53 +3,85 @@ import ReactDOM from 'react-dom';
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
-import RoundedImage from "react-rounded-image";
-import SidebarImage from './images/logo192.png';
-import TempImage from './images/temp.png';
+import Image from "react-bootstrap/Image";
+import ProjectImg from "react-rounded-image"
+import { TagCloud } from 'react-tagcloud';
+import Badge from 'react-bootstrap/Badge';
+
 import {Container, Row, Col} from 'react-bootstrap';
 import Sidebar from "react-sidebar";
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function arrFormat(a) {
-  let result = "" + a[0];
-  for (let i = 1; i < a.length; i++) {
-    result += ", " + a[i];
-  }
-  return result;
-}
+import SidebarImage from './images/logo192.png';
+import TempImage from './images/temp.png';
+import FridayImg from './images/friday.png'
+import MedClusterImg from './images/medcluster.png';
+import MajMinImg from './images/majmin.png';
+import PokerImg from './images/poker.jpg';
+import FrcImg from './images/frc.png';
 
 const ABOUT = {
-  INTRO: "Welcome! I'm a Software Engineer, primaryly interested in backend development, but I'm never afraid to learn new technologies. I love coding, math, and heavy weight.",
-  HEADLINE: "Software Engineer",
+  INTRO: "Welcome! I'm primarly interested in backend development, but always trying to learn new tech. I love coding, math, and lifting heavy weights.",
+  BUILT: "This website was made using React.", //arrow to github
+  HEADLINE: "Software Developer",
   EMAIL: "pwamsley2019@gmail.com",
   GITHUB: "pwamsley2015.github.com",
   INSTA: "https://www.instagram.com/patrick_wamsley/",
 };
 
+const LANGS = [
+  {value: "Java", count: 50},
+  {value: "C", count: 25},
+  {value: "Python", count: 15},
+  {value: "SQL", count: 20},
+  {value: "JavaScript", count: 20}
+];
+
+const TECH = [
+  {value: "React", count: 20},
+  {value: "Git", count: 40},
+  {value: "JSON", count: 23},
+  {value: "Maven", count: 20},
+  {value: "HTML", count: 20},
+  {value: "LaTeX", count: 25},
+];
+
+const TOOLS = [
+  {value: "UML Diagrams", count: 33},
+  {value: "Github", count: 40},
+  {value: "Slack", count: 20},
+  {value: "Eclipse", count: 35},
+  {value: "IntelliJ IDEA", count: 25}
+];
+
 const PROJECTS = [
   { name: "Friday",
+    img: FridayImg,
     code: "https://github.com/pwamsley2015/public_friday",
-    about: "My personal assistant chatbot which automates tasks, logs data, manages my schedule, and keeps me hacking. Dynamic IO over the web and local intranet.",
+    about: "My personal assistant chatbot which automates tasks, logs data, manages my schedule, and keeps me hacking. Dynamic IO accomplished over the web and local intranet.",
     roles: ["Everything"], 
     built_with: ["Twilio Messaging API", "Java", "Python", "Spark", "JavaFX"]
   },
   { name: "Congressional Redistricting",
+    img: MajMinImg,
     code: "https://github.com/pwamsley2015/congressional_redistricting",
     about: "Web application which generates congressional redistricting lines, built to maximize the number of majority-miniority distrcts.",
     roles: ["Primary Backend Developer", "Designed and implemented AlogrithmStep framework to modularize Alogrithm", "Implemented Graph Partioning Algorithm to generate majority-miniority distrcts"],
-    built_with: ["React", "TypeScript", "Java", "Hibernate"]
+    built_with: ["Angular", "TypeScript", "Java", "Hibernate"]
   },
   {
     name: "MedCluster",
+    img: MedClusterImg,
     code: "https://github.com/pwamsley2015/medcluster",
     about: "Machine Learning and Data Visualization tool used to optimize EMT base station locations.",
-    roles: ["Primary Backend Developer", "Implemented k-means clustering Algorithm"],
+    roles: ["Backend Developer", "Implemented k-means clustering Algorithm"],
     built_with: ["Python", "Google Maps API", "JavaScript"]
   },
   {
     name: "No Limit Hold'Em Equity Calculator",
+    img: PokerImg,
     code: "https://github.com/pwamsley2015/NLHE_lab",
     about: "Calculates Range vs Range Equity in Poker.",
     roles: ["Everything"],
@@ -57,7 +89,8 @@ const PROJECTS = [
   }, 
   {
     name: "FRC 2485 Robotics",
-    code: "https://github.com/team2485/frc-2016",
+    img: FrcImg,
+    code: "https://github.com/team2485/frc-2015",
     about: "Autonomous and Teleoperated Robot Control for FRC Recycle Rush",
     roles: ["Lead Developer"],
     built_with: ["Java", "WPI FRC Framework"]
@@ -72,45 +105,61 @@ class ProjectDisplay extends React.Component {
 
   render() {
     const project = this.props.project;
-    const l = "{";
-    const r = "}";
-    let rolesFormat = arrFormat(project.roles);
-    let builtWithFormat = arrFormat(project.built_with);
+    let builtWith = [];
+    builtWith.push(<Badge variant="primary">Built with:</Badge>);
+    for (let i = 0; i < project.built_with.length; i++) {
+      builtWith.push(<Badge pill variant="info">{project.built_with[i]}</Badge>);
+    }
+
+    const builtWithTags = [];
+    for (let i = 0; i < project.built_with.length; i++) {
+      builtWithTags.push({value: project.built_with[i], count: Math.floor(Math.random() * 30) + 15});
+    }
 
     return (
-      <Card style={{ width: '18rem'}}>
-        <Card.Img src={TempImage} alt="Card image" />
-        <Card.ImgOverlay>
-          <Card.Body>
-            <Card.Title>{project.name}</Card.Title>
-            <Card.Text>{project.about}</Card.Text>
-          </Card.Body>
-          <Card.Body>
-            <Card.Text>Roles: {rolesFormat}</Card.Text>
-          </Card.Body>
-          <Card.Body>
-            <Card.Text>Built with: {builtWithFormat}</Card.Text>
-          </Card.Body>
-          <Card.Body>
-            <a href={project.code}>{l}code{r}</a>
-          </Card.Body>
-        </Card.ImgOverlay>
-      </Card>
+      <Container style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Title>
+                <h1 style={{"textAlign":"center"}}>{project.name}</h1>
+              </Card.Title>
+              <Card.Body>
+                <ProjectImg 
+                  image={project.img} 
+                  roundedSize="8"
+                />
+                <Button href={project.code} target="_blank" variant="dark">See the code</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Row style={{"textAlign":"center", "font-size":"19px"}}>{project.about}</Row>
+            <Row>
+              <Col>
+                <Badge style={{"font-size":"25px"}} variant="success" >Built with:</Badge>
+              </Col>
+              <Col>
+                <TagCloud tags={builtWithTags} minSize={12} maxSize={22}/>
+              </Col>
+            </Row>
+          </Col>
+          </Row>
+          </Container>
     );
   }
 }
 
 class ProjectPage extends React.Component {
   render() {
-
     const items = [];
     for (var i = 0; i < PROJECTS.length; i++) {
       items.push(<ProjectDisplay key={i} project={PROJECTS[i]}/>);
     }
-
-    return ( 
+    return (
       <div>
-        {items}
+        <h1>Projects</h1>
+      {items}
       </div>
     );
   }
@@ -120,12 +169,42 @@ class AboutSection extends React.Component {
   render() {
     return (
       <div className="about_section">
-        <Container>
-          <Row>
-            <Col>Skills</Col>
-            <Col>Education</Col> 
-          </Row>
-        </Container>
+        <CardDeck>
+          <Card>
+            <Card.Title>
+              Languages
+            </Card.Title>
+            <Card.Body>
+              <TagCloud
+                tags={LANGS}
+              />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Title>
+              Tech
+            </Card.Title>
+            <Card.Body>
+              <TagCloud
+                tags={TECH}
+              />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Title>
+              Tools
+            </Card.Title>
+            <Card.Body>
+              <TagCloud
+                tags={TOOLS}
+              />
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Title>BS Computer Science</Card.Title>
+            <Card.Body>Stony Brook University</Card.Body>
+          </Card>
+        </CardDeck>
       </div>
     );
   }
@@ -135,25 +214,18 @@ class SideBarContent extends React.Component {
   render() {
 
     return (
-      <div className="sidebar">
-        <Container>
-          <Col>
-            <Row>
-              <RoundedImage
-                 image={SidebarImage}
-                 imageWidth="150"
-                 imageHeight="150"
-                 roundedSize="13"
-              />
-            </Row>
-           
-            <Row>
-              <h3>{ABOUT.HEADLINE}</h3>
-            </Row>
-            <Row>{ABOUT.INTRO}</Row>
-            <div> <b>Links</b> <br/> </div>
-          </Col>
-        </Container>
+      <div className="sidebar" style={{
+        "text-align":"center"
+      }}>
+        <Image
+           src={SidebarImage}
+           roundedCircle
+        />
+     
+        <h3>{ABOUT.HEADLINE}</h3>
+      
+        {ABOUT.INTRO}
+      Links
       </div>
     );
   }
@@ -162,32 +234,51 @@ class SideBarContent extends React.Component {
 class Banner extends React.Component {
   render() {
     return <div className="banner">
-      <Container>
-        <Col>
-          <Row>
-            <h1>Patrick Wamsley</h1>
-          </Row>
-          <Row> 
-            <Button variant="outline-primary">See Projects</Button>{' '} 
-          </Row>
-        </Col>
-      </Container>
+      <h1 style= {{  
+          "text-align":"center", 
+          "font-size":"80px"
+        }}
+      > Patrick Wamsley </h1>
     </div>
   }
 }
 
 class HomeScreen extends React.Component {
   render() {
-    return <div className="home_screen">
-      <Banner /> 
-        <Sidebar
-        sidebar={<SideBarContent/>}
-        docked={false}
-        styles={{ sidebar: { background: "white" } }}
-      />
-      <AboutSection />
-      <ProjectPage />
-    </div>
+    return (
+      <div className="home_screen" style = {{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+        <Container>
+          <Row>
+            <Col>
+              <Sidebar
+                sidebar={<SideBarContent/>}
+                docked={true}
+                styles={{ sidebar: { 
+                          background: "white",
+                          position: "fixed", 
+                          top: 0,
+                          bottom: 0,
+                          transition: "transform .3s ease-out",
+                          willChange: "transform",
+                          overflowY: "auto",
+                          width: "300px"
+                        }
+                      }}
+              />
+            </Col>
+            <Col xs={8}>
+              <Banner />
+              <AboutSection />
+              <ProjectPage />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 
