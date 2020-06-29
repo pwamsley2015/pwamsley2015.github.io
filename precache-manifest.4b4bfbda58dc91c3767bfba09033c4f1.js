a142164c0596316069ed6c97cc0aa3131fcc94e1
Patrick Wamsley <pwamsley2015@francisparker.org>
2020-06-29 14:33:48 -0700

    Updates

---
+++ precache-manifest.4b4bfbda58dc91c3767bfba09033c4f1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8b7e71da5e7883e18717057bed328a",
+    "url": "/pwamsley2015.github.io/index.html"
+  },
+  {
+    "revision": "239264a29daaa0dbbdfd",
+    "url": "/pwamsley2015.github.io/static/css/2.5dfc0601.chunk.css"
+  },
+  {
+    "revision": "8c3992605186cf848162",
+    "url": "/pwamsley2015.github.io/static/css/main.445602be.chunk.css"
+  },
+  {
+    "revision": "239264a29daaa0dbbdfd",
+    "url": "/pwamsley2015.github.io/static/js/2.3574fced.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/pwamsley2015.github.io/static/js/2.3574fced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3992605186cf848162",
+    "url": "/pwamsley2015.github.io/static/js/main.bbd7aba0.chunk.js"
+  },
+  {
+    "revision": "368b3333e057a6bb903e",
+    "url": "/pwamsley2015.github.io/static/js/runtime-main.50a1cb18.js"
+  },
+  {
+    "revision": "82b23c0243ef8b336ed2b77dd3f0ab25",
+    "url": "/pwamsley2015.github.io/static/media/frc.82b23c02.png"
+  },
+  {
+    "revision": "d2f543fdaf351d0a652ffef616bf035d",
+    "url": "/pwamsley2015.github.io/static/media/friday.d2f543fd.png"
+  },
+  {
+    "revision": "3b1a1e65b9c29139c88a78e49044e311",
+    "url": "/pwamsley2015.github.io/static/media/majmin.3b1a1e65.png"
+  },
+  {
+    "revision": "f4d6fae2d2948810a83fdfa16ebd7e81",
+    "url": "/pwamsley2015.github.io/static/media/medcluster.f4d6fae2.png"
+  },
+  {
+    "revision": "f807a50a23a05050bc5701fa2de8e5ca",
+    "url": "/pwamsley2015.github.io/static/media/poker.f807a50a.jpg"
+  },
+  {
+    "revision": "5c6066e6a24ffc757c99b4175fd75025",
+    "url": "/pwamsley2015.github.io/static/media/sbu.5c6066e6.png"
+  },
+  {
+    "revision": "2bf2d41140e34a6707985ca356f625a5",
+    "url": "/pwamsley2015.github.io/static/media/site.2bf2d411.png"
+  },
+  {
+    "revision": "13127f4d5a432d72fd1f012ec24e44ee",
+    "url": "/pwamsley2015.github.io/static/media/temp.13127f4d.png"
+  }
+]);
